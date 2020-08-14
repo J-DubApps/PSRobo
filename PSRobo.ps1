55d0cdc5142166ee079bb3fc303873711a5e6543
Julian West <jdub.writes.code@gmail.com>
2020-08-13 23:50:43 -0500

    Update PSRobo.ps1

--- PSRobo.ps1
+++ PSRobo.ps1
@@ -1,4 +1,21 @@
-﻿param (
+<#
+Powershell-Robocopy
+       
+.SYNOPSIS
+
+Welcome to PSRobo, this takes the guess work out of RoboCopy command line arguments.
+
+.DESCRIPTION
+
+Copying .\PSRobo.ps1 -Operation Copy -CACLs (if needing NTFS file level permissions) -DaysOld 1095 (if only needing +3yr old data or older) -DaysNew 3 (if only needing 3 day old data or newer) -Source DriveLetter:\FOLDER1\FOLDER2\PARENT\SOURCE -Target DriveLetter:\FOLDER1\FOLDER2\PARENT
+
+Moving .\PSRobo.ps1 -Operation Move -CACLs (if needing NTFS file level permissions) -DaysOld 1095 (if only needing +3yr old data or older) -DaysNew 3 (if only needing 3 day old data or newer) -Source DriveLetter:\FOLDER1\FOLDER2\PARENT\SOURCE -Target DriveLetter:\FOLDER1\FOLDER2\PARENT
+
+Deleting .\PSRobo.ps1 -Operation DELTREE (Recursively delete Source directory and files) -DaysOld 1095 (if only needing +3yr old data or older) -DaysNew 3 (if only needing 3 day old data or newer) -Source DriveLetter:\FOLDER1\FOLDER2\PARENT\SOURCE
+
+#>
+
+param (
        [ValidateSet('Help','Copy','Move','DELTREE','GUI')]
        [Parameter(
                   Position=0,
